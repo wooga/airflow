9804cefd0af445f32bc3da152879feade0409d9b
Jan Omar <jan.omar@wooga.net>
2015-07-26 13:41:54 +0200

    use custom execute logic for now

--- airflow/operators/exasol_operator.py
+++ airflow/operators/exasol_operator.py
@@ -1,7 +1,9 @@
 __author__ = 'janomar'
 from airflow.operators.jdbc_operator import JdbcOperator
+from airflow.hooks.jdbc_hook import JdbcHook
 from airflow.utils import apply_defaults
 
+import logging
 
 class ExasolOperator(JdbcOperator):
     """
@@ -11,3 +13,11 @@ class ExasolOperator(JdbcOperator):
     def __init__(self, retries=35 ,exasol_conn_id='exasol_default', *args, **kwargs):
         super(ExasolOperator,self).__init__(retries=retries, jdbc_conn_id=exasol_conn_id, *args,**kwargs)
 
+     # TODO find out if why we use get_records instead of run here, it's a really bad idea
+     # get_records should be side effect free, right now that's not the case... it shouldn't require any kind of commit
+     # fix autocommit somehow, if we have to use get_records - try to avoid exasol specific hook if possible..
+    def execute(self, context):
+        logging.info('Executing: ' + self.sql)
+        self.hook = JdbcHook(jdbc_conn_id=self.jdbc_conn_id)
+        for row in self.hook.get_records(self.sql, self.autocommit):
+            logging.info('Result: ' + ','.join(map(str,row)) )
