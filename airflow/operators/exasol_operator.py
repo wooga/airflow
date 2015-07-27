2e6d9de488c3b860283468c41d7ae54d9db939d7
Jan Omar <jan.omar@wooga.net>
2015-07-27 10:23:10 +0200

    remove exasol specific execution logic

--- airflow/operators/exasol_operator.py
+++ airflow/operators/exasol_operator.py
@@ -13,11 +13,14 @@ class ExasolOperator(JdbcOperator):
     def __init__(self, retries=35 ,exasol_conn_id='exasol_default', *args, **kwargs):
         super(ExasolOperator,self).__init__(retries=retries, jdbc_conn_id=exasol_conn_id, *args,**kwargs)
 
-     # TODO find out if why we use get_records instead of run here, it's a really bad idea
+     # Don't use get_records for now. We may have to investigate our options if we need output from executing
+     # scripts in exasol again.
+     #
+     #
      # get_records should be side effect free, right now that's not the case... it shouldn't require any kind of commit
      # fix autocommit somehow, if we have to use get_records - try to avoid exasol specific hook if possible..
-    def execute(self, context):
-        logging.info('Executing: ' + self.sql)
-        self.hook = JdbcHook(jdbc_conn_id=self.jdbc_conn_id)
-        for row in self.hook.get_records(self.sql, self.autocommit):
-            logging.info('Result: ' + ','.join(map(str,row)) )
+     #def execute(self, context):
+     #    logging.info('Executing: ' + self.sql)
+     #    self.hook = JdbcHook(jdbc_conn_id=self.jdbc_conn_id)
+     #    for row in self.hook.get_records(self.sql, self.autocommit):
+     #        logging.info('Result: ' + ','.join(map(str,row)) )
