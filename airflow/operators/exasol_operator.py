79b7f059a478d2aeb8456949c0722ba48d4199c3
Cedrik Neumann <cedrik.neumann@wooga.net>
2015-07-28 11:55:59 +0200

    add missing retry_delay and cleanup

--- airflow/operators/exasol_operator.py
+++ airflow/operators/exasol_operator.py
@@ -1,17 +1,16 @@
 __author__ = 'janomar'
 from airflow.operators.jdbc_operator import JdbcOperator
-from airflow.hooks.jdbc_hook import JdbcHook
 from airflow.utils import apply_defaults
+from datetime import timedelta
 
-import logging
 
 class ExasolOperator(JdbcOperator):
     """
     Alias for backwards compatibility
     """
     @apply_defaults
-    def __init__(self, retries=35 ,exasol_conn_id='exasol_default', *args, **kwargs):
-        super(ExasolOperator,self).__init__(retries=retries, jdbc_conn_id=exasol_conn_id, *args,**kwargs)
+    def __init__(self, retries=35, retry_delay=timedelta(seconds=300), exasol_conn_id='exasol_default', *args, **kwargs):
+        super(ExasolOperator,self).__init__(retries=retries, retry_delay=retry_delay, jdbc_conn_id=exasol_conn_id, *args,**kwargs)
 
      # Don't use get_records for now. We may have to investigate our options if we need output from executing
      # scripts in exasol again.
