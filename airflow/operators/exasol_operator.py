c3b2fb16f51a90c2bec12f5c0d2bd7fca286c8b3
Cedrik Neumann <cedrik.neumann@wooga.net>
2015-07-29 11:11:45 +0200

    adjust default autocommit

--- airflow/operators/exasol_operator.py
+++ airflow/operators/exasol_operator.py
@@ -9,17 +9,5 @@ class ExasolOperator(JdbcOperator):
     Alias for backwards compatibility
     """
     @apply_defaults
-    def __init__(self, retries=35, retry_delay=timedelta(seconds=300), exasol_conn_id='exasol_default', *args, **kwargs):
-        super(ExasolOperator,self).__init__(retries=retries, retry_delay=retry_delay, jdbc_conn_id=exasol_conn_id, *args,**kwargs)
-
-     # Don't use get_records for now. We may have to investigate our options if we need output from executing
-     # scripts in exasol again.
-     #
-     #
-     # get_records should be side effect free, right now that's not the case... it shouldn't require any kind of commit
-     # fix autocommit somehow, if we have to use get_records - try to avoid exasol specific hook if possible..
-     #def execute(self, context):
-     #    logging.info('Executing: ' + self.sql)
-     #    self.hook = JdbcHook(jdbc_conn_id=self.jdbc_conn_id)
-     #    for row in self.hook.get_records(self.sql, self.autocommit):
-     #        logging.info('Result: ' + ','.join(map(str,row)) )
+    def __init__(self, retries=35, retry_delay=timedelta(seconds=300), autocommit=True, exasol_conn_id='exasol_default', *args, **kwargs):
+        super(ExasolOperator,self).__init__(retries=retries, retry_delay=retry_delay, autocommit=autocommit, jdbc_conn_id=exasol_conn_id, *args,**kwargs)
